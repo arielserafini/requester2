a3e53f0eefecbeb2c2c035b53e0e3132408d0c66
Ariel Serafini <git@arielserafini.com>
2016-09-06 02:12:29 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -3,51 +3,84 @@ import logo from './logo.svg';
 import './App.css';
 
 class RequestList extends Component {
-    render() {
-        let addRequestField = (item) => {
-            return (
-                <RequestField key={item.id} uri={item.uri}/>
-            );
-        }
-        return (
-            <div>
-                {this.props.requests.map(addRequestField)}
-            </div>
-        );
+  render() {
+    let addRequestField = (item) => {
+      return (
+        <li>
+          <RequestPreset key={item.id} uri={item.uri} method={item.method} name={item.name} onClick={setActivePreset(item)}/>
+        </li>
+      );
     }
+    return (
+      <ul>
+        {this.props.requests.map(addRequestField)}
+      </ul>
+    );
+  }
 }
 
-class RequestField extends Component {
+class RequestPreset extends Component {
+  render() {
+    return (
+      <a>{this.props.name}</a>
+    );
+  }
+};
 
-    state = {
-        uri: this.props.uri
-    }
+class URIField extends Component {
 
-    updateRequestURI(e){
-        this.setState({
-            uri: e.target.value
-        });
-    }
+  state = {
+    uri: this.props.uri,
+    method: this.props.method
+  }
 
-    render() {
-        return (
-            <div><input value={this.state.uri} onChange={this.updateRequestURI.bind(this)} />{this.state.uri}</div>
-        );
-    }
+  updateRequestURI(e){
+    this.setState({
+      uri: e.target.value
+    });
+  }
+
+  render() {
+    let addMethodOption = (item) => {
+      return (
+        <option value={item} key={item}>{item}</option>
+      );
+    };
+
+    return (
+      <div>
+        <select>
+          {this.props.methods.map(addMethodOption)}
+        </select>
+        <input value={this.state.uri} onChange={this.updateRequestURI.bind(this)} />{this.state.uri}
+      </div>
+    );
+  }
 }
 
 class App extends Component {
   state = {
     APP_NAME: 'requester',
-    requests: []
+    HTTP_METHODS: [
+      'GET', 'POST'
+    ],
+    requests: [],
   }
 
   addRequest() {
-    let requests = this.state.requests.concat([{uri: '', id: Date.now()}]);
+    let requests = this.state.requests.concat([{
+      uri: '', 
+      id: Date.now(),
+      name: 'New Request'
+    }]);
 
     this.setState({
-        requests: requests
+      requests: requests
     });
+  }
+
+  setActivePreset(preset) {
+    console.log(preset);
   }
 
 
@@ -57,10 +90,10 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>{this.state.APP_NAME}</h2>
-          <button onClick={this.addRequest.bind(this)}>Add request</button>
-          <RequestList requests={this.state.requests} />
         </div>
-
+        <URIField methods={this.state.HTTP_METHODS} />
+        <button onClick={this.addRequest.bind(this)}>Add request</button>
+        <RequestList requests={this.state.requests} />
       </div>
     );
   }
